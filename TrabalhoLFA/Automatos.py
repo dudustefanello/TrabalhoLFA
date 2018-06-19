595bbef3740a1a5b56880af001975cd587222e13
dudustefanello <eduardo@stefanello.cc>
2018-06-19 20:53:12 -0300

    Documentação da Classe do Automato

--- TrabalhoLFA/Automatos.py
+++ TrabalhoLFA/Automatos.py
@@ -1,16 +1,17 @@
 from Producoes import Producoes;
-from Regras import Regras;
+from Estados import Estados;
 
-class Automatos(object):
+class Automato(object):
 
     Automato = [];
     
     def __init__(self, arquivo):
         # -- Leitura de arquivos:
-        self.Automato = [];
-        arquivo = open(arquivo, 'r');
-        texto = arquivo.read();
-        self.carrega(texto);
+        
+        self.Automato = [];             # Inicializa o vetor da classe
+        arquivo = open(arquivo, 'r');   # Abre o arquivo de entrada
+        texto = arquivo.read();         # Lê o arquivo de entrada
+        self.carrega(texto);            # Carrega o automato a partir do arquivo de texto
 
 
     def carrega(self, texto):
@@ -18,32 +19,41 @@ class Automatos(object):
 
         New = True;
 
-        self.Automato.append(Regras(0));
+        self.Automato.append(Estados(0));  # Insere o estado final
 
         for a in texto:
-            a = a.lower();  # Colocar letra em minusculo
-            if a == '\n':   # Identificar quebras de linha
-                New = True; # Flag de nova palavra
+            a = a.lower();   # Colocar letra em minusculo
+           
+            if a == '\n':                       # Identificar quebras de linha
+                New = True;                     # Flag de nova palavra
+                self.Automato[-1].Final = True; # Marca o Estado como Final
             else:
                 if New:
                     self.Automato[0].AddLigacao(a, len(self.Automato)); # Carrega no estado inicial
-                    self.Automato.append(Regras(len(self.Automato)));   # Cria o estado para continuação
+                    self.Automato.append(Estados(len(self.Automato)));  # Cria o estado para continuação
+
                     New = False;
                 else:
                     self.Automato[-1].AddLigacao(a, len(self.Automato)); # Insere no último estado criado
-                    self.Automato.append(Regras(len(self.Automato)));    # Cria o estado para continuação 
+                    self.Automato.append(Estados(len(self.Automato)));   # Cria o estado para continuação 
     
                     
     def imprimeTela(self):
         # -- Impressão do Automato
-        for i in self.Automato:
-            if len(i.Producoes) == 0:
-                i.Final = True;
-                print('*', end='');
+
+        for i in self.Automato: # Loop no automato
+            if i.Final:
+                print('*', end=''); # Marca asterisco para estados que são finais
             else:
-                print(' ', end='');
-            print('<' + str(i.Identificador) + '> ::= | ', end='');
-            for j in i.Producoes:
-                print(j.Token + '<' + str(j.Destino) + '> | ', end='');
-            print('');
+                print(' ', end=''); # Coloca espaço para compesar asterisco
+            
+                print('<' + str(i.Identificador) + '> ::= | ', end=''); # Insere o identificador da regra
+            
+            for j in i.Producoes:                                       # Loop nas produções do estado selecionado
+                print(j.Token + '<' + str(j.Destino) + '> | ', end=''); # Insere as produções
+            
+            print('');  # Insere quebra de linha ao final de cada estado            
+
+
+                
 
