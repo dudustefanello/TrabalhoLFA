18161ca6a1f2a76832f694407e3ba8211fd855b6
dudustefanello <eduardo@stefanello.cc>
2018-06-24 20:38:43 -0300

    Comentário e Otimização
    Refiz os comentários, identei bonitinho e otimizei alguns laços e procedimentos.

--- TrabalhoLFA/Automatos.py
+++ TrabalhoLFA/Automatos.py
@@ -1,108 +1,121 @@
 class Automato(object):
+    
+    # -- Declaração dos campos da classe
+    Estados = dict();   # Estrutura que guarda todos os estados do autômato
+    Alfabeto = set();   # Estrutura que contém todos os símbolos do alfabeto
+    Finais = set();     # Estrutura que contém os estados que são finais
+    Texto = str();      # Campo para guardar a string de entrada
 
-    Estados = dict();
-    Alfabeto = set();
-
-    # -- Leitura de arquivos:
+    # -- Inicialização da classe:
     def __init__(self, arquivo):
-
-        self.Estados = {};  # Inicializa o dictionary da classe
+        self.Estados = dict();          # Inicializa o dictionary de estados com uma estrutura vazia
+        self.Alfabeto = set();          # Inicializa o set do alfabeto com uma estrutura vazia
+        self.Finais = set();            # Inicializa o set de estados finais com uma estrutura vazia
 
         arquivo = open(arquivo, 'r');   # Abre o arquivo de entrada
-        texto = arquivo.read();         # Lê o arquivo de entrada
-        self.carrega(texto);            # Carrega o automato a partir do arquivo de texto
-
-        self.setAlfabeto();             # Relacioanar os estados com o Alfabeto
-
-        self.buscarIndeterminismo();
-
-        self.setAlfabeto();             # Relacioanar os estados com o Alfabeto
-
-        self.imprimir();                # Imprimir 
+        self.Texto = arquivo.read();    # Converte o arquivo para string
 
 
     # -- Inserção de Tokens da Linguagem
-    def carregaTokens(self, word, New):
-        self.Alfabeto.add(word);
-        if New:
-            if word in self.Estados[0]:                                # Se o Token já existe no estado
-                self.Estados[0][word].append(len(self.Estados));       # Então adiciona referenciando o token
-            else:
-                self.Estados[0].update({word: [len(self.Estados)]});   # Senão carrega no estado inicial, criando novo token
+    def carregaToken(self, simbolo, new):
+        self.Alfabeto.add(simbolo);                                                     # Adiciona o símbolo ao conjunto alfabeto
 
-            self.Estados.update({len(self.Estados):{}});            # Cria o próximo estado para continuação
-        else:
-            self.Estados[len(self.Estados) - 1].update({word: [len(self.Estados)]});   # Insere no último estado criado
-            self.Estados.update({len(self.Estados):{}});                            # Cria o próximo estado para continuação
+        if new:                                                                         # Se o símbolo for de um novo token:                                                                        
+            if simbolo in self.Estados[0]:                                              # Se o token já existir no estado:
+                self.Estados[0][simbolo].append(len(self.Estados));                     # Adiciona na lista de estados destinos daquele token
+
+            else:                                                                       # Se o token ainda não existir no estado:
+                self.Estados[0].update({simbolo: [len(self.Estados)]});                 # Adiciona uma nova entrada para o token
+            self.Estados.update({len(self.Estados):{}});                                # Cria um estado vazio para a próxima iteração
+
+        if not new:                                                                     # Se o símbolo for de um token já existente:
+            self.Estados[len(self.Estados) - 1].update({simbolo: [len(self.Estados)]}); # Insere no último estado criado
+            self.Estados.update({len(self.Estados):{}});                                # Cria um estado vazio para a próxima iteração
+
+        self.setAlfabeto();                                                             # Relaciona os estados com o alfabeto da linguagem
 
 
     # -- Inserção das regras da Gramática Regular
     def carregaGramatica(self, word):
+        # Implementar a leitura de gramatica
         pass
 
 
+    # -- Imprime o automato finito deterministico
     def imprimir(self):
-        for i in range(0, len(self.Estados)): 
-            print(i, end=' = ')
-            for j in sorted(self.Alfabeto):
-                if len(self.Estados[i][j]) > 0:
-                    print(j, self.Estados[i][j], end=', ');
-            print('')
+        for nome, estado in self.Estados.items():           # Faz um loop nos estados
+            print(nome, end=' = ');                         # Imprime o nome/numero do estado
+
+            for simbolo, trasicoes in estado.items():       # Faz um loop em cada estado
+                if len(trasicoes) > 0:                      # Se existir transições para um símbolo
+                    print(simbolo, trasicoes, end=', ');    # Imprime o símbolo e a lista de transições
+
+            print('');                                      # Insere uma quebra de linha ao final de cada impressão de símbolo
 
 
-    # -- Relacioanar os estados com o Alfabeto
-    def setAlfabeto(self):
-        for i in range(0, len(self.Estados)):  
-            for j in self.Alfabeto:
-                if j not in self.Estados[i]:
-                    self.Estados[i].update({j:[]})
-            
+    # -- Insere todos os símbolos do alfabeto em um estado
+    def setAlfabetoEstado(self, estado):
+        for simbolo in sorted(self.Alfabeto):   # Faz um loop nos símbolos do alfabeto da linguagem
+            if simbolo not in estado:           # Se o símbolo não existir no estado:
+                estado.update({simbolo:[]});    # Adiciona o símbolo associado à uma lista vazia.
+
+
+    # -- Relacioana os estados com os símbolos do alfabeto
+    def setAlfabeto(self):   
+        for nome, estado in self.Estados.items():   # Faz um loop nos estados
+            self.setAlfabetoEstado(estado);         # Relacioana o estado com os símbolos do alfabeto
         
 
-    # -- Inserção no automato:
-    def carrega(self, texto):
+    # -- Insere no automato:
+    def carrega(self):
+        new = True;                                     # Marca flag de novo estado
+        gramatica = False;                              # Marca a flag de tipo de leitura
 
-        New = True;
+        self.Estados.update({len(self.Estados):{}});    # Inicializa o estado inicial com:
+                                                        # um identificador inteiro para chave e
+                                                        # um dicionário vazio para conteúdo
+        
+        for simbolo in self.Texto:                      # Faz um loop, caractere e caractere na string da estrada
 
-        self.Estados.update({len(self.Estados):{}});
+            simbolo = simbolo.lower();                  # Utiliza todas as letras em minusculo
 
-        for a in texto:
-            a = a.lower();  # Utiliza letra em minusculo
-           
-            if a == '\n':   # Identifica quebra de linha
-                New = True;
-                #self.carregaGramatica(a);
-            else:
-                self.carregaTokens(a, New);
-                New = False;
+            if not gramatica:                           # Identifica que a leitura é de tokens da linguagem
+                if simbolo == '\n':                     # Identifica quebra de linha
+                    gramatica = new;                    # Identifica linha em branco e passa a ler gramáticas
+                    new = True;                         # Marca flag para informar que o próximo símbolo é o início de um novo token
+                else:                                   # Se não for quebra de linha:
+                    self.carregaToken(simbolo, new);    # Carrega o token para o automato.
+                    new = False;                        # Reseta a variável para o próximo símbolo
+
+            if gramatica:                               # Identifica que a leitura é de gramática regular.
+                pass;
+
+    # -- Determiniza um estado do automato
+    def determinizar(self, producoes):
+        estadoTemporario = dict();                                                              # Cria um estado temporário
+        novoEstado = len(self.Estados);                                                         # Dá nome/número ao estado que será criado
+
+        for i in producoes:                                                                     # Faz um loop nas produções que que contém a indeterminação
+            for j in sorted(self.Alfabeto):                                                     # Faz um loop no conjunto de símbolos do alfabeto
+                if j in estadoTemporario:                                                       # Se o o símbolo j já estiver no estado temporário:
+                    estadoTemporario[j] = list(set(estadoTemporario[j] + self.Estados[i][j]))   # Adiciona a lista de transições de j ao estado
+                else:                                                                           # Senão:
+                    estadoTemporario.update({j:list(set(self.Estados[i][j]))});                 # Atualiza o estado temporário com o símbolo j e com a lista já existente no estado
+
+        self.setAlfabetoEstado(estadoTemporario);                                               # Relaciona o estado temporário com os símbolos do alfabeto
+        self.Estados.update({novoEstado:estadoTemporario});                                     # Adiciona o estado temporário ao dicionário de estados da classe
+        
+        return [novoEstado];                                                                    # Renorna o nome/número do estado adicionado
 
 
-
-    def determinizar(self, producao):
-        lista = [];
-        temp = dict();
-        novo = len(self.Estados);
-        for i in producao:
-            for j in sorted(self.Alfabeto):
-                if len(self.Estados[i][j]) > 0:
-                    if j in temp:
-                        temp[j] = list(set(temp[j] + self.Estados[i][j]))
-                    else:
-                        temp.update({j:list(set(self.Estados[i][j]))});
-
-        self.Estados.update({novo:temp})
-
-        return [novo];
-
-
+    # -- Percorre o autômato identificando e tratando seus indeterminismos
     def buscarIndeterminismo(self):
-        quantidadeEstados = len(self.Estados);
-        i = 0;
+        qtdEstados = len(self.Estados);                                         # Marca a quantidade inicial de estados do autômato
+        i = 0;                                                                  # Zera a variável de índice
+        while i < qtdEstados:                                                   # Faz um loop pelos estados
+            for j in sorted(self.Alfabeto):                                     # Itera um laço pelo conjunto de símbolos do alfabeto
+                if len(self.Estados[i][j]) > 1:                                 # Se houver uma transição estiver indeterminada:
+                    self.Estados[i][j] = self.determinizar(self.Estados[i][j]); # Determiniza o estado
 
-        while i < quantidadeEstados:
-            for j in sorted(self.Alfabeto):
-                if len(self.Estados[i][j]) > 1:
-                    self.Estados[i][j] = self.determinizar(self.Estados[i][j]);
-                    self.setAlfabeto();             # Relacioanar os estados com o Alfabeto
-                    quantidadeEstados = len(self.Estados);
-            i += 1;
+                    qtdEstados = len(self.Estados);                             # Atualiza a quantidade de estados
+            i += 1;                                                             # Incrmenta o índice.
--- TrabalhoLFA/TrabalhoLFA.py
+++ TrabalhoLFA/TrabalhoLFA.py
@@ -1,6 +1,10 @@
 from Automatos import Automato;
 
-Indeterministico = Automato('liguagem.txt');
+Indeterministico = Automato('liguagem.txt');    #Incializa o automato com o arquivo de entrada
+
+Indeterministico.carrega();                 # Carrega o automato a partir da string
+Indeterministico.buscarIndeterminismo();    # Busca indeterminismos para determinizar
+Indeterministico.imprimir();                # Imprimir automato final
                 
 
 
