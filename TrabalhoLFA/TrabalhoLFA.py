721cacdb7ecce25d5459f0b0894fa631d9ba5fe7
Laurivan Sareta <laurivansareta@gmail.com>
2018-07-02 01:07:03 -0300

    Remoção do épsolon transições. Correção para gravar o estado final na importação da gramática.

--- TrabalhoLFA/TrabalhoLFA.py
+++ TrabalhoLFA/TrabalhoLFA.py
@@ -3,7 +3,6 @@ from Automatos import Automato;
 automato = Automato('liguagem.txt');    # Incializa o automato com o arquivo de entrada
 
 automato.carrega();                     # Carrega o automato a partir da string
+automato.buscarEpsilonTransicoes();     # Busca os épsilon transições e trata as mesmas;
 automato.buscarIndeterminismo();        # Busca indeterminismos para determinizar
 automato.imprimir();                    # Imprimir automato final
-
-              
