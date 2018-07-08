0fdb868080b3616ac5fa3bd6211a9eefa51b3590
dudustefanello <eduardo@stefanello.cc>
2018-07-08 16:11:59 -0300

    Substituição da função leitura

--- TrabalhoLFA/TrabalhoLFA.py
+++ TrabalhoLFA/TrabalhoLFA.py
@@ -2,9 +2,7 @@ from Determinizacao import Determinizacao;
 from Automatos import Automato;
 
 automato = Automato();    # Incializa o automato com o arquivo de entrada
-automato.leitura('liguagem.txt')
-
-automato.carrega();                     # Carrega o automato a partir da 
+automato.carrega('liguagem.txt');                     # Carrega o automato a partir da 
 automato.imprimir('\n\n######################## AUTOMATO LIDO: ########################\n'); # Imprimir automato final
 
 automato.buscarEpsilonTransicoes();     # Busca os épsilon transições e trata as mesmas;
@@ -13,7 +11,6 @@ automato.imprimir('\n\n################## LIVRE DE EPSILON TRANSIÇÕES: #######
 determinizado = Determinizacao(automato)
 determinizado.determinizar();
 determinizado.imprimir('\n\n######################## DETERMINIZADO: ########################\n'); # Imprimir automato final
-
 
 automato.removerInalcancaveis();
 automato.imprimir('\n\n###################### SEM INALCANÇAVEIS: ######################\n'); # Imprimir automato final
