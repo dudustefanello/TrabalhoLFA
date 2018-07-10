3befdc7a4137372181c1ef35f1d684716dd5867b
Eduardo Stefanello <eduardostefanello3@gmail.com>
2018-07-10 20:40:04 -0300

    Comentários no programa principal

--- TrabalhoLFA/TrabalhoLFA.py
+++ TrabalhoLFA/TrabalhoLFA.py
@@ -1,26 +1,26 @@
-from EspsilonTransicao import EspsilonTransicao;
-from Determinizacao import Determinizacao;
-from Inalcancaveis import Inalcancaveis;
-from Automatos import Automato;
-from Mortos import Mortos;
+from EpsilonTransicao import EpsilonTransicao   # Importa a implementação da eliminação de épsilon transições
+from Determinizacao import Determinizacao       # Importa a implementação da determinização
+from Inalcancaveis import Inalcancaveis         # Importa a implementação da remoção de inalcançáveis
+from Automatos import Automato                  # Importa as implementações gerais do automato
+from Mortos import Mortos                       # Importa a implementação de remoção de mortos
 
-automato = Automato();    # Incializa o automato com o arquivo de entrada
-automato.carrega('liguagem.txt');                     # Carrega o automato a partir da 
-automato.imprimir('\n\n# AUTOMATO LIDO:\n'); # Imprimir automato final
+automato = Automato()                           # Instancia a classe Automato
+automato.carrega('liguagem.txt')                # Carrega o automato a partir do arquivo de texto
+automato.imprimir('\n\n# AUTOMATO LIDO:\n')     # Imprime automato lido
 
-livreEpsilon = EspsilonTransicao(automato)
-livreEpsilon.eliminarEpsilonTransicoes();     # Busca os épsilon transições e trata as mesmas;
-livreEpsilon.imprimir(); # Imprimir automato final
+livreEpsilon = EpsilonTransicao(automato)       # Instancia o objeto para remover epsilons transicões
+livreEpsilon.eliminarEpsilonTransicoes()        # Busca as épsilon transições e trata as mesmas
+livreEpsilon.imprimir()                         # Imprimir automato livre de épsilon transições
 
-determinizado = Determinizacao(automato)
-determinizado.determinizar();
-determinizado.imprimir(); # Imprimir automato final
+determinizado = Determinizacao(automato)        # Instancia o objeto para determinização
+determinizado.determinizar()                    # Faz a determinização do autômato
+determinizado.imprimir()                        # Imprimir automato determinizado
 
-semInalcancaveis = Inalcancaveis(automato)
-semInalcancaveis.removerInalcancaveis();
-semInalcancaveis.imprimir(); # Imprimir automato final
+semInalcancaveis = Inalcancaveis(automato)      # Instancia o objeto para remover estados inalcançáveis
+semInalcancaveis.removerInalcancaveis()         # Remove os estados inalcançáveis
+semInalcancaveis.imprimir()                     # Imprimir automato final
 
-semInalcancaveis = Mortos(automato)
-semInalcancaveis.removerMortos();
-semInalcancaveis.imprimir(); # Imprimir automato final
+semInalcancaveis = Mortos(automato)             # Instancia o objeto para remover estados mortos
+semInalcancaveis.removerMortos()                # Remove os estados mortos
+semInalcancaveis.imprimir()                     # Imprime automato minimizado
 
