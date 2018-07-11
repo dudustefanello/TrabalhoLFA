6a75b8ecb162a250e6f2fbb56c4387b7008a8b24
Eduardo Stefanello <eduardo@stefanello.cc>
2018-07-11 11:58:34 -0300

    Comentários e Documentação

--- TrabalhoLFA/Determinizacao.py
+++ TrabalhoLFA/Determinizacao.py
@@ -2,7 +2,7 @@ from Automatos import Automato
 
 class Determinizacao(Automato):
     '''Classe que implementa a determinização de um automato'''
-    
+
     # -- Inicialização da Classe
     def __init__(self, automato):
         super(Determinizacao, self).__init__()
@@ -10,30 +10,30 @@ class Determinizacao(Automato):
         self.Estados = automato.Estados
         self.Alfabeto = automato.Alfabeto
         self.Finais = automato.Finais
-        
-    
+
+
     # -- Chama a função imprimir da classe pai
     def imprimir(self):
         return super().imprimir('\n\n# DETERMINIZADO:\n')
 
 
-    # -- Inicia o processo de determinização 
+    # -- Inicia o processo de determinização
     def determinizar(self):
         self.buscarIndeterminismo() # Busca indeterminismos para determinizar
-      
-        
+
+
     # -- Adiciona uma nova produção ao conjunto de estados finais
     def adicionaEstadoFinal(self, producoes, novaProducao):
         for producao in producoes:              # Loop nas produções
             if producao in self.Finais:         # Para cada produção que for final
                 self.Finais.add(novaProducao)   # Adiciona a nova produção também ao conjunto de finais
 
-              
-    # -- Substitui um indeterminismo por uma string concatenando as transições                
+
+    # -- Substitui um indeterminismo por uma string concatenando as transições
     def substituiNovaProducao(self):
         if len(self.NovosEstados) > 0:                                                                  # Se foram criadas novas produções na determinização
             for estado in self.Estados:                                                                 # Para cada estado do automato
-                for simbolo in sorted(self.Alfabeto):                                                   # e para cada símbolo do alfabeto 
+                for simbolo in sorted(self.Alfabeto):                                                   # e para cada símbolo do alfabeto
                     if len(self.Estados[estado][simbolo]) > 1:                                          # se a quantidade de transições por uma produção foi maior que 1
                         producaoAgrupada = self.geraProducaoAgrupada(self.Estados[estado][simbolo])     # gera uma "Produção Agrupada", para se conhecer a origem da nova produção
                         if producaoAgrupada in self.NovosEstados:                                       # Se essa produção estiver nas novas produções
@@ -51,7 +51,7 @@ class Determinizacao(Automato):
 
                     qtdEstados = len(self.Estados)                                      # Atualiza a quantidade de estados
             i += 1                                                                      # Incrmenta o índice.
-            
+
             if i == qtdEstados:                                                         # Se a iteração estiver no último estado
                 for novoEstado in list(self.NovosEstados):                              # Percorre a lista de novas produções
                     if self.NovosEstados[novoEstado][0] not in self.Estados:            # Se ela não estiver no conjunto de estados do automato
@@ -60,8 +60,8 @@ class Determinizacao(Automato):
 
 
     # -- Determiniza um estado do automato
-    def determinizarProducao(self, producoes):        
-        estadoTemporario = dict()                                                                                               
+    def determinizarProducao(self, producoes):
+        estadoTemporario = dict()
         producaoAgrupada = self.geraProducaoAgrupada(producoes)
 
         if ((producaoAgrupada not in self.NovosEstados) or                                                      # Se a prudução agrupada não existir nas novas produções
@@ -72,30 +72,30 @@ class Determinizacao(Automato):
 
             for i in producoes:                                                                                 # Faz um loop nas produções que que contém a indeterminação
                 for j in sorted(self.Alfabeto):                                                                 # Fazendo loop no conjunto de símbolos do alfabeto
-                    if j in estadoTemporario:                                                                   # Se o o símbolo j já estiver no estado temporário:                        
-                        lista = list(set(estadoTemporario[j] + self.pegarProducaoOriginal(self.Estados[i][j]))) # Adiciona a lista de transições de j
+                    if j in estadoTemporario:                                                                   # Se o o símbolo j já estiver no estado temporário:
+                        lista = list(set(estadoTemporario[j] + self."(self.Estados[i][j]))) # Adiciona a lista de transições de j
                         estadoTemporario[j] = lista                                                             # ao estado criado
-                    
+
                         if (len(lista) > 1) and (not self.existeProducaoAgrupada(lista)):                       # Se a largura dessa lista for maior que 1 e não existe produção agrupada pra ela
                             novoEstado = self.pegarNovoEstadoDetrminizacao()                                    # Enumera um novo estado
                             self.NovosEstados.update({self.geraProducaoAgrupada(lista): [novoEstado,lista]})    # Adicina aos novos estados
 
                     else:                                                                                       # Se o o símbolo j não estiver no estado temporário:
                         producaoAtual = list(set(self.Estados[i][j]))                                           # Concatena as transições de j
-                        
+
                         if len(producaoAtual) > 1:                                                              # Se tiver mais de uma produção
                             producaoAgrupadaTemp = self.geraProducaoAgrupada(producaoAtual)                     # Gera uma nova produção agrupada
                             if self.existeProducaoAgrupada(producaoAgrupadaTemp):                               # e se essa produção já existe
                                 estadoTemporario.update({j: list(set(self.NovosEstados[producaoAgrupada][1]))}) # Atualiza o estado temporário
-                        
+
                         elif (len(producaoAtual) > 0) and (self.existeNovoEstado(producaoAtual[0])):            # se não se essa proodução não for nula e existe um estado para essa transição
                             for prod in self.NovosEstados:                                                      # Efetua um loop nesses novos estados
                                 if self.NovosEstados[prod][0] == producaoAtual[0]:                              # e para o estado que for igual à transição
                                     estadoTemporario.update({j: list(set(self.NovosEstados[prod][1]))})         # Atualiza o estado
                         else:                                                                                   # Senão
                             estadoTemporario.update({j: producaoAtual})                                         # Adiciona uma nova transição ao estado temporário
-        
-            self.setAlfabetoEstado(estadoTemporario)                                                            # Relaciona o estado temporário com os símbolos do alfabeto                    
+
+            self.setAlfabetoEstado(estadoTemporario)                                                            # Relaciona o estado temporário com os símbolos do alfabeto
             self.Estados.update({self.NovosEstados[producaoAgrupada][0]: estadoTemporario})                     # Adiciona o estado temporário ao dicionário de estados da classe
             self.adicionaEstadoFinal(producoes, self.NovosEstados[producaoAgrupada][0])                         # Verifica se deve adicionar aos estados finais
             self.substituiNovaProducao()                                                                        # Verifica as produção criadas
@@ -109,17 +109,17 @@ class Determinizacao(Automato):
                 estado += str(item)         # Concatena o item
             else:                           # se for no meio
                 estado += ',' + str(item)   # concatena com a vpirgula
-                        
+
         return estado                       # Retorna a string
 
 
     # -- Verifica se uma lista dada já foi inserida em um estado novo
-    def existeProducaoAgrupada(self, lista):    
+    def existeProducaoAgrupada(self, lista):
         retorno = False
 
         if len(lista) > 1:                                          # Se o tamanho da lista dada for maior que um
-            producaoAgrupadaTemp = self.geraProducaoAgrupada(lista) # Verifica pela sua possível produção agrupada  
-            return (producaoAgrupadaTemp in self.NovosEstados)      
+            producaoAgrupadaTemp = self.geraProducaoAgrupada(lista) # Verifica pela sua possível produção agrupada
+            return (producaoAgrupadaTemp in self.NovosEstados)
         else:                                                       # Senão
             for i in self.NovosEstados:                             # Percorre os novos estados
                 if lista[0] in self.NovosEstados[i][1]:             # Verificando se a produção já existe
@@ -130,11 +130,12 @@ class Determinizacao(Automato):
     # -- Adiciona uma nova produção para contabilizar o novo tamanho do automato
     def pegarNovoEstadoDetrminizacao(self):
         novasProducoes = []
-        for producao in self.NovosEstados:                          # Para cada produção dos novos estados   
+        for producao in self.NovosEstados:                          # Para cada produção dos novos estados
             novasProducoes.append(self.NovosEstados[producao][0])   # Adicona um na contagem
         return len(set(list(self.Estados) + novasProducoes))        # E retorna a quantidade. Utiliza lista para tratar repetições
-   
-    
+
+
+    # -- Retorna a produção que está na lista de estados novos a partir de uma produção do automato
     def pegarProducaoOriginal(self, producaoOrig):
         retorno = list(set(producaoOrig))
         for producao in self.NovosEstados:
@@ -143,8 +144,9 @@ class Determinizacao(Automato):
                     retorno = list(set(self.NovosEstados[producao][1]))
 
         return retorno
-    
 
+
+    # -- Verifica se uma produção dada está no conjunto de novos estados
     def existeNovoEstado(self, producao):
         if len(self.NovosEstados) > 0:
             for producaoAgrupada in self.NovosEstados:
